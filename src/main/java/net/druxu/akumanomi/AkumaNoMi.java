7e1380dafcf38fb97998f29fdabe45aeef3e4eff
andrewca79 <andrewcaruanaavc@gmail.com>
2023-02-06 15:42:08 +0100

    Initial commit

---
+++ src/main/java/net/druxu/akumanomi/AkumaNoMi.java
@@ -0,0 +1,16 @@
+package net.druxu.akumanomi;
+
+import net.fabricmc.api.ModInitializer;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+public class AkumaNoMi implements ModInitializer {
+
+	public static final String MOD_ID = "akuma_no_mi";
+	public static final Logger LOGGER = LoggerFactory.getLogger(MOD_ID);
+
+	@Override
+	public void onInitialize() {
+
+	}
+}
---
+++ src/main/java/net/druxu/akumanomi/AkumaNoMiClient.java
@@ -0,0 +1,10 @@
+package net.druxu.akumanomi;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class AkumaNoMiClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
